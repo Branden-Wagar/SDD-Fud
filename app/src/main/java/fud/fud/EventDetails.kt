e66ccbe80e430093f1691fd066c3905798a393cf
veitej <jkveitengruber@gmail.com>
2018-10-11 15:58:37 -0400

    ui stuff

---
+++ app/src/main/java/fud/fud/EventDetails.kt
@@ -0,0 +1,15 @@
+package fud.fud
+
+import android.os.Bundle
+import android.app.Activity
+
+import kotlinx.android.synthetic.main.activity_event_details.*
+
+class EventDetails : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_details)
+    }
+
+}
