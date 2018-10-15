80a215b4d5cb1fe02716033ecd8bbbf5ad93294b
Philip Kamei <kameip@rpi.edu>
2018-10-14 21:32:02 -0400

    Added fields to the Event class.

--- app/src/main/java/fud/fud/Models/Event.java
+++ app/src/main/java/fud/fud/Models/Event.java
@@ -3,12 +3,16 @@ package fud.fud.Models;
 import java.util.Date;
 
 public class Event {
-    public Date date;
-    public String address;
+    public Date endDate;        /* Includes time */
+    public String address;      /* Longitude and latitude might be more helpful here */
+    public float longitude;
+    public float latitude;
+    public String name;
     public String description;
     public String cuisineType;
-    public double distanceToUser;
+    public double distanceToUser;   /* Will be updated periodically with a listener */
     public double price;
+    public boolean isSponsored;     /* True if the event is sponsored, false otherwise */
 
     // TODO: Implement User class eventually
     // private User user;
