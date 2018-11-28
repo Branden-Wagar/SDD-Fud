5bcbb8956bf0e7600d76387772d19b7e081803c8
Branden-Wagar <wagarb@rpi.edu>
2018-11-27 23:59:45 -0500

    Location data available for CreateEventVM and MainActivityVM

--- app/src/main/java/fud/fud/MainActivityVM.kt
+++ app/src/main/java/fud/fud/MainActivityVM.kt
@@ -1,12 +1,19 @@
 package fud.fud
 
+import android.app.Activity
 import android.arch.lifecycle.LiveData
 import android.content.Context
 import android.content.Intent
+import android.content.Context.LOCATION_SERVICE
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.databinding.BaseObservable
 import android.databinding.Bindable
 import android.databinding.Observable
 import android.databinding.ObservableField
+import android.location.Location
+import android.location.LocationManager
+import android.support.v4.content.ContextCompat.checkSelfPermission
+import android.support.v4.content.ContextCompat.getSystemService
 import android.support.v4.content.ContextCompat.startActivity
 import android.widget.ArrayAdapter
 import android.widget.ListView
@@ -15,6 +22,7 @@ import com.google.android.gms.tasks.TaskCompletionSource
 import com.google.firebase.firestore.FirebaseFirestore
 import fud.fud.Database.DatabaseManager
 import fud.fud.Models.Event
+import fud.fud.Models.LocationManagerLocal
 import kotlin.math.roundToInt
 
 class MainActivityVM( ct : Context) : BaseObservable() {
@@ -23,10 +31,19 @@ class MainActivityVM( ct : Context) : BaseObservable() {
     var FreeFoodFilter = ObservableField<String>("Free Only")
     var EventsListAdapter = ObservableField<ArrayAdapter<String>>()
     var FoodTypeFilters = ObservableField<ArrayAdapter<String>>(ArrayAdapter<String>(ct, android.R.layout.simple_list_item_1, ct.resources.getStringArray(R.array.food_tags)))
-    var parentContest = ct
     var eventString = arrayListOf<String>()
     var events = arrayListOf<Event>()
+    var parentContext = ct
 
+    var locManager : LocationManagerLocal = LocationManagerLocal.instance
+    val locationManager = parentContext.getSystemService(Activity.LOCATION_SERVICE) as LocationManager
+
+
+    init{
+        if (locationManager.isLocationEnabled && checkSelfPermission(parentContext, android.Manifest.permission.ACCESS_FINE_LOCATION) == PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5, 5.1.toFloat(), locManager, null)
+        }
+    }
     private var maxEventPrice : Double = 0.0
 
     private var _freeFoodOnly = false
@@ -89,7 +106,9 @@ class MainActivityVM( ct : Context) : BaseObservable() {
         var eventsL = ArrayList<Event>()
         var localEvents = ArrayList<String>()
         maxEventPrice = 0.0
-        //val eventString = arrayListOf<String>()
+
+        var location : Location? = getCurrLocation()
+
         //get the information for each of the events
         dbManager.allEvents.addOnCompleteListener { task ->
             if (task.isSuccessful()) {
@@ -110,13 +129,43 @@ class MainActivityVM( ct : Context) : BaseObservable() {
         }
         eventString = localEvents
         events = eventsL
-        EventsListAdapter.set(ArrayAdapter(parentContest, android.R.layout.simple_list_item_1, localEvents))
+        EventsListAdapter.set(ArrayAdapter(parentContext, android.R.layout.simple_list_item_1, localEvents))
 
 
 
     }
+    private fun calculateDistance(l1 : Location?, l2 : Location?) : Double{
 
-    private fun filterPrice(lim : Double) {
+        if (l1 == null || l2 == null){
+            return 0.0
+        }
+        var a = Math.abs(l1.latitude - l2.latitude)
+        var b = Math.abs(l1.longitude - l2.longitude)
+        // a^2 + b^2 = c^2
+        var distance = Math.pow(a, 2.0) + Math.pow(b, 2.0)
+        distance = Math.sqrt(distance)
+        return distance
+    }
+
+
+    private fun getCurrLocation() : Location?{
+        return if (locationManager.isLocationEnabled && checkSelfPermission(parentContext, android.Manifest.permission.ACCESS_FINE_LOCATION) == PERMISSION_GRANTED){
+            val providers = locationManager.getProviders(true)
+            var bestLocation: Location? = null
+            for (provider in providers) {
+                val l = locationManager.getLastKnownLocation(provider) ?: continue
+                if (bestLocation == null || l.accuracy < bestLocation.accuracy) {
+                    // Found best last known location: %s", l);
+                    bestLocation = l
+                }
+            }
+            bestLocation
+        }
+        else{
+            null
+        }
+    }
+    private fun filterPrice(lim : Double){
         var dbInstance = FirebaseFirestore.getInstance()
         var dbManager = DatabaseManager(dbInstance)
         events.clear()
@@ -141,11 +190,15 @@ class MainActivityVM( ct : Context) : BaseObservable() {
             }
             events = eventLocal
             eventString = eventStringLocal
-            EventsListAdapter.set(ArrayAdapter(parentContest, android.R.layout.simple_list_item_1, eventStringLocal))
+            EventsListAdapter.set(ArrayAdapter(parentContext, android.R.layout.simple_list_item_1, eventStringLocal))
         }
+
+
     }
 
 
 
 
 }
+
+
