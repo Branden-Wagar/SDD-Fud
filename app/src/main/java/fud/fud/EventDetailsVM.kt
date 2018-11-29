a1c8a3623d3449d9f678b3166db7f2d373d3ddef
veitej <jkveitengruber@gmail.com>
2018-11-29 14:00:23 -0500

    some comments

--- app/src/main/java/fud/fud/EventDetailsVM.kt
+++ app/src/main/java/fud/fud/EventDetailsVM.kt
@@ -11,11 +11,12 @@ import fud.fud.Models.Event
 
 class EventDetailsVM (event:Event): ViewModel() {
 
-
+    //get the values from the event
     val name = event.getEventName()
     val distance = event.getDistanceToUser()
     val discription = event.getDescription()
     val price = event.getPrice()
+    //bind these values to the page
     var filter1 = ObservableField<String>(name)
     var filter2= ObservableField<String>(distance.toString()+" miles")
     var filter3=ObservableField<String>(discription)
